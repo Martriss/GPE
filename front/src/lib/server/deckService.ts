a4e4c637ffc5e7b055f2d595bed804e7611d22ba
mouton <savary_r@etna-alternance.net>
2025-05-21 22:45:51 +0200

    add com

--- front/src/lib/server/deckService.ts
+++ front/src/lib/server/deckService.ts
@@ -3,6 +3,7 @@ import { addDoc, collection } from "firebase/firestore";
 
 export async function createUserDeck(userUuid: string) {
   // Add a new document with a generated id.
+  // Savoir si addDoc permet de le générer même si ${userUuid} n'est pas encore créé
   const docRef = await addDoc(collection(firestore, `users/${userUuid}/decks`), {
     name: "Tokyo",
     country: "Japan"
