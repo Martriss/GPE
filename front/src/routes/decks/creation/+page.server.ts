9af2ad8d378d29b2adab65f78d39bdde9d8952d5
mouton <romain.savary@etu.u-pec.fr>
2025-05-22 14:44:42 +0200

    fix

--- front/src/routes/decks/creation/+page.server.ts
+++ front/src/routes/decks/creation/+page.server.ts
@@ -5,56 +5,56 @@ import type DeckType from '$lib/interfaces/DeckType';
 import { fail, redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async () => {
-	return {
-		// rulesets: await rulesetService.getAllRulesets()
-		rulesets: [{ uuid: 'zhPZBo2kuOXpxdXMuXwk', name: "Magic The Gathering" }] // mock pour le développement afin d'éviter le nombre de lecture
-	};
+  return {
+    // rulesets: await rulesetService.getAllRulesets()
+    rulesets: [{ uuid: 'zhPZBo2kuOXpxdXMuXwk', name: "Magic The Gathering" }] // mock pour le développement afin d'éviter le nombre de lecture
+  };
 };
 
 export const actions = {
-	createDeck: async ({ request }) => {
-		const data = await request.formData();
-		const name = data.get("name")?.toString();
-		const game = data.get("game")?.toString();
-		const visibility = data.get("visibility")?.toString();
-		// ajouter plus tard pour récupérer les "cartes" à ajouter au deck à la création
-		// description is optionnel, no need to check
+  createDeck: async ({ request }) => {
+    const data = await request.formData();
+    const name = data.get("name")?.toString();
+    const game = data.get("game")?.toString();
+    const visibility = data.get("visibility")?.toString();
+    // ajouter plus tard pour récupérer les "cartes" à ajouter au deck à la création
+    // description is optionnel, no need to check
 
-		if (!name) {
-			return fail(400, { name, isMissing: true, isEmpty: true });
-		}
-		if (!game) {
-			return fail(400, { name, isMissing: true, isEmpty: true });
-		}
-		if (!visibility) {
-			return fail(400, { name, isMissing: true, isEmpty: true });
-		}
+    if (!name) {
+      return fail(400, { name, isMissing: true, isEmpty: true });
+    }
+    if (!game) {
+      return fail(400, { name, isMissing: true, isEmpty: true });
+    }
+    if (!visibility) {
+      return fail(400, { name, isMissing: true, isEmpty: true });
+    }
 
-		// Comportement par défaut de visibilité lors de la création d'un deck
-		let isPublic: boolean = false;
-		let isShared: boolean = false;
-		if (visibility === "public")
-			isPublic = true;
-		if (visibility === "unlisted")
-			isShared = true;
+    // Comportement par défaut de visibilité lors de la création d'un deck
+    let isPublic: boolean = false;
+    let isShared: boolean = false;
+    if (visibility === "public")
+      isPublic = true;
+    if (visibility === "unlisted")
+      isShared = true;
 
-		const deck: DeckType = {
-			name,
-			description: data.get("description")?.toString(),
-			isPublic,
-			isShared,
-			userId: "f2ECMFu1RYXMLEFvrnyFSHnkRth1", // replace later the user uuid,
-			rulesetId: game,
-			cards: [] // view later to add cards at the creation time
-		};
+    const deck: DeckType = {
+      name,
+      description: data.get("description")?.toString(),
+      isPublic,
+      isShared,
+      userId: "f2ECMFu1RYXMLEFvrnyFSHnkRth1", // replace later the user uuid,
+      rulesetId: game,
+      cards: [] // view later to add cards at the creation time
+    };
 
-		let deckId: string = "0";
-		try {
-			deckId = await deckService.createUserDeck(deck);
-		} catch (error) {
-			console.error(error);
-			return fail(500, { message: "Erreur lors de la création du deck. Le deck n'a pas pu être crée", isError: true });
-		}
-		redirect(303, `/decks/${deckId}`);
-	}
+    let deckId: string = "0";
+    try {
+      deckId = await deckService.createUserDeck(deck);
+    } catch (error) {
+      console.error(error);
+      return fail(500, { message: "Erreur lors de la création du deck. Le deck n'a pas pu être crée", isError: true });
+    }
+    redirect(303, `/decks/${deckId}`);
+  }
 } satisfies Actions;
