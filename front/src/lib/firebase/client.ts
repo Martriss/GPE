845e8220f6bb0227872202235b01654def19e31b
Ovvvs <62432963+Ovvvs@users.noreply.github.com>
2025-05-22 14:45:55 +0200

    auth (#9)
    * auth
    
    * fix
    
    ---------
    
    Co-authored-by: mouton <romain.savary@etu.u-pec.fr>

--- front/src/lib/firebase/client.ts
+++ front/src/lib/firebase/client.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInAnonymously, signInWithEmailAndPassword } from "firebase/auth";
 import { FIREBASE_APIKEY, FIREBASE_AUTHDOMAIN, FIREBASE_PROJECTID, FIREBASE_STORAGEBUCKET, FIREBASE_MESSAGINGSENDERID, FIREBASE_APPID, FIREBASE_AUTH_EMAIL, FIREBASE_AUTH_PASSWORD } from "$env/static/private";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -31,11 +31,21 @@ export const firestore = getFirestore(firebaseApp);
 
 // réfléchir pour le faire dans un service lorsque l'authentification sera en place
 export const auth = getAuth(firebaseApp);
-signInWithEmailAndPassword(auth, FIREBASE_AUTH_EMAIL, FIREBASE_AUTH_PASSWORD)
-  .then(userCredential => {
-    // console.log("Utilisateur connecté :", userCredential.user);
-    console.log("Utilisateur bien connecté");
-  })
-  .catch(error => {
-    console.error("Erreur de connexion :", error);
-  });
+if (FIREBASE_AUTH_EMAIL && FIREBASE_AUTH_PASSWORD) {
+  signInWithEmailAndPassword(auth, FIREBASE_AUTH_EMAIL, FIREBASE_AUTH_PASSWORD)
+    .then(userCredential => {
+      // console.log("Utilisateur connecté :", userCredential.user);
+      console.log("Utilisateur bien connecté");
+    })
+    .catch(error => {
+      console.error("Erreur de connexion :", error);
+    });
+} else {
+  signInAnonymously(auth)
+    .then(() => {
+      console.log("Utilisateur anonyme connecté");
+    })
+    .catch(error => {
+      console.error("Erreur de connexion d'utilisateur anonyme :", error);
+    })
+}
