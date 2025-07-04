b14e1e380967cf7d8fbe1930cd8bc8ab731044c2
mouton <savary_r@etna-alternance.net>
2025-07-04 16:08:09 +0200

    SCRUM-54 create register function in firebase/auth.ts

--- front/src/lib/firebase/auth.ts
+++ front/src/lib/firebase/auth.ts
@@ -1,4 +1,4 @@
-import { signInWithEmailAndPassword, signOut, type UserCredential } from "@firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, validatePassword, type UserCredential } from "@firebase/auth";
 import { auth } from "./client";
 import type { Cookies } from "@sveltejs/kit";
 
@@ -32,5 +32,28 @@ export async function logout(cookies: Cookies): Promise<void> {
     console.log("User disconnected");
   } catch (err) {
     throw new Error("Enabled to disconnect the user from firebase");
+  }
+}
+
+/**
+ * Pour inscrire un utilisateur à firebase à l'aide d'une email et d'un mot de passe. L'inscription connecte automatique l'utilisateur. Jete une erreur si l'inscription échoue
+ * @param cookies les cookies du serveur. C'est utilisé afin de sauvegarder l'id de l'utilisateur qui vient de se connecté dans un cookie sécurisé
+ * @param email email de l'utilisateur
+ * @param password password de l'utilisateur
+ */
+export async function registerWithEmailAndPassword(cookies: Cookies, email: string, password: string): Promise<void> {
+  // Vérification du mot de passe
+  const status = await validatePassword(auth, password);
+  if (!status.isValid) {
+    throw new Error("Password does not comply with security rules");
+  }
+
+  try {
+    const userCredential: UserCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+    cookies.set('user_id', userCredential.user.uid, { path: '/' });
+    console.log("User register and connected");
+  } catch (err) {
+    throw new Error("Enabled to register the user to firebase");
   }
 }
