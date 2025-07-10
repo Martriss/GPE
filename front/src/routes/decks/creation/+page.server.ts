593992bedbe105640a6b79e52e9cb834340fc729
Ovvvs <62432963+Ovvvs@users.noreply.github.com>
2025-07-10 21:29:35 +0200

    SCRUM-58 normalisation form (#14)
    * SCRUM-58 normalisation form
    
    * Feature/scrum 54 authentification (#13)
    
    * SCRUM-54 add logout and loginWithEmailAndPassword
    
    * SCRUM-54 creation fonction to connect and disconnect + getTheCurrentUser (in cookies for server side) (in store for client side)
    
    * SCRUM-54 begin of the updated navbar if the user is connected or not
    
    * SCRUM-54 update .env.exemple
    
    * SCRUM-54 update nav bar when user is connected or not
    
    * SCRUM-54 add page signin, hook to check if user is connected or not
    
    * SCRUM-54 responsive
    
    * SCRUM-54 begin of the signup page
    
    * SCRUM-54 clean .env.exemple
    
    * SCRUM-54 create register function in firebase/auth.ts
    
    * SCRUM-54 fix cursor in password field
    
    * SCRUM-54 register function
    
    ---------
    
    Co-authored-by: mouton <romain.savary@etu.u-pec.fr>
    
    * SCRUM-58 normalisation form
    
    * add comment
    
    ---------
    
    Co-authored-by: mouton <romain.savary@etu.u-pec.fr>

--- front/src/routes/decks/creation/+page.server.ts
+++ front/src/routes/decks/creation/+page.server.ts
@@ -17,24 +17,21 @@ export const actions = {
     const data = await request.formData();
     const name = data.get("name")?.toString();
     const game = data.get("game")?.toString();
+    const description = data.get("description")?.toString();
     const visibility = data.get("visibility")?.toString();
     // ajouter plus tard pour récupérer les "cartes" à ajouter au deck à la création
-    // description is optionnel, no need to check
 
-    if (!name) {
-      return fail(400, { name, isMissing: true, isEmpty: true });
-    }
-    if (!game) {
-      return fail(400, { name, isMissing: true, isEmpty: true });
-    }
-    if (!visibility) {
-      return fail(400, { name, isMissing: true, isEmpty: true });
+    // Test les variables obligatoires ensemble parce que dans le message d'erreur côté front le message sera identique
+    // description is optionnel, no need to check
+    if (!name || !game || !visibility) {
+      return fail(400, { name, game, description, visibility, isMissing: true });
     }
 
     const userCurrentId = cookies.get('user_id');
     if (!userCurrentId) {
       // Normalement nous ne sommes jamais sensé arrivé dedans, parce que l'utilisateur doit être connecté pour accéder à la page de création
       // Essayer de voir pour afficher un message d'erreur si jamais l'on rentre ici
+      // Voir une redirection ? Error 401 ?
       error(500);
     }
 
@@ -48,10 +45,10 @@ export const actions = {
 
     const deck: DeckType = {
       name,
-      description: data.get("description")?.toString(),
+      description,
       isPublic,
       isShared,
-      userId: userCurrentId || '', // Plus haut il y a une vérification de le cookie ne soit pas vide 
+      userId: userCurrentId || '', // Plus haut il y a une vérification afin de s'assurer que le cookie ne soit pas vide
       rulesetId: game,
       cards: [] // view later to add cards at the creation time
     };
@@ -61,7 +58,7 @@ export const actions = {
       deckId = await createUserDeck(deck);
     } catch (error) {
       console.error(error);
-      return fail(500, { message: "Erreur lors de la création du deck. Le deck n'a pas pu être crée", isError: true });
+      return fail(500, { message: "Erreur lors de la création du deck. Le deck n'a pas pu être crée", isError: true, name, game, description, visibility });
     }
     redirect(303, `/decks/${deckId}`);
   }
