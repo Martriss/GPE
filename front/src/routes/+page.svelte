f8daebde67a7e8f8cb40c1a30c30e2cbd2d2781c
mouton <savary_r@etna-alternance.net>
2025-08-31 17:01:07 +0200

    BIK-60 add buttons and title in the home page

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
+<script>
+  import ButtonFilled from "$lib/components/Button/ButtonFilled.svelte";
+</script>
+
+<div class="flex flex-col mx-4 my-8">
+  <div class="flex justify-center mt-[15vh]">
+    <h1 class="h1 text-center">Le Grimoire Infini</h1>
+  </div>
+  <div class="flex flex-col gap-[7vh] mt-[20vh]">
+    <ButtonFilled name="JOUER" handleClick={(e) => {}} />
+    <ButtonFilled name="REJOINDRE" handleClick={(e) => {}} />
+  </div>
+</div>
