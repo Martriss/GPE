939ff208e73c1c249162879c0042114273339f2e
Romain SAVARY <savary_r@etna-alternance.net>
2025-09-04 10:24:41 +0200

    SCRUM-61 nettoyage du service pour ajouter une zone

--- front/src/lib/server/services/zoneService.ts
+++ front/src/lib/server/services/zoneService.ts
@@ -2,6 +2,14 @@ import { firestore } from "$lib/firebase/client";
 import type ZoneType from "$lib/interfaces/ZoneType";
 import { addDoc, collection } from "firebase/firestore";
 
-export async function addZoneToRuleset(zone: ZoneType, rulesetId: string) {
-  await addDoc(collection(firestore, "rulesets"), { name: "TEST", createdBy: "lwI2AkazCybXQYspiuREQootJBf2" });
+/**
+ * Pour ajouter une zone à une ruleset
+ * @param zone zone avec ses informations. Attend un ZoneType
+ * @param rulesetId l'id du ruleset dans lequel on veut ajouter la zone
+ * @returns l'id de la zone nouvellement crééé
+ */
+export async function addZoneToRuleset(zone: ZoneType, rulesetId: string): Promise<string> {
+  const docRef = await addDoc(collection(firestore, "rulesets", rulesetId, "zones"), zone);
+
+  return docRef.id;
 }
--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,9 +1,5 @@
-<script>
-    import DividerWithText from "$lib/components/Divider/DividerWithText.svelte";
-    import ButtonSubmitCustom from "$lib/components/Form/ButtonSubmitCustom.svelte";
-</script>
-
 <h1>Welcome to SvelteKit</h1>
-<form method="POST" action="?/createZones">
-    <ButtonSubmitCustom name="ADD ZONE" />
-</form>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
